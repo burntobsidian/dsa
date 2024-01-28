9ec06848e32a625f543d95419f0ea260d996c01c
burntobsidian <me@burntobsidian.com>
2024-01-28 17:13:03 +0530

    liner search

--- src/day1/LinearSearchList.ts
+++ src/day1/LinearSearchList.ts
@@ -1,3 +1,8 @@
 export default function linear_search(haystack: number[], needle: number): boolean {
-
+  for (let idx = 0; idx < haystack.length; idx++) {
+    if (haystack[idx] === needle) {
+      return true;
+    }
+  }
+  return false
 }
