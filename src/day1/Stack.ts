31a59d6d36f0c5fb4e0ebc6aa67473e023a6eeaa
burntobsidian <me@burntobsidian.com>
2024-01-31 00:02:32 +0530

    stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,29 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+};
+
 export default class Stack<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
 
     constructor() {
+        this.length = 0;
+        this.head = undefined;
     }
 
     push(item: T): void {
-
-}
+        this.length++;
+        this.head = { value: item, next: this.head };
+    }
     pop(): T | undefined {
-
-}
+        if (!this.head) return;
+        this.length--;
+        const poppedNode = this.head;
+        this.head = poppedNode.next;
+        return poppedNode.value;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
