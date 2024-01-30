b90da7a6f461afe4c3e0809a9ddb787d09527112
burntobsidian <me@burntobsidian.com>
2024-01-30 23:51:07 +0530

    queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,42 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+};
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.length = 0;
+        this.head = this.tail = undefined;
     }
 
     enqueue(item: T): void {
+        this.length++;
+        if (!this.tail) {
+            this.head = this.tail = { value: item };
+            return;
+        }
 
-}
+        this.tail.next = { value: item };
+        this.tail = this.tail?.next;
+    }
+
     deque(): T | undefined {
+        if (!this.head || !this.tail) return;
 
-}
+        this.length--;
+        const deletedNode = this.head;
+        this.head = this.head.next;
+        if (this.length === 0) {
+            this.tail = undefined;
+        }
+        return deletedNode.value;
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
