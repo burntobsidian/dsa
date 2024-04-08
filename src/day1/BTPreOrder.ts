8d8366b4fe3695b61695ed43dd82f8a825bfab2d
burntobsidian <me@burntobsidian.com>
2024-04-08 23:44:03 +0530

    tree traversal pre order

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,17 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {
+function walk(node: BinaryNode<number> | null, path: number[]): number[] {
+    if (!node) {
+        return path;
+    }
 
+    // pre
+    path.push(node.value);
+
+    //recurse
+    walk(node.left, path);
+    walk(node.right, path);
+
+    return path;
+}
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
