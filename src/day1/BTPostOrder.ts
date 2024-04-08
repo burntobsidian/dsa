927e362d23ad93300411c272d47a5c682c6879e3
burntobsidian <me@burntobsidian.com>
2024-04-08 23:47:47 +0530

    tree traversal post order

--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,3 +1,18 @@
-export default function post_order_search(head: BinaryNode<number>): number[] {
+function walk(node: BinaryNode<number> | null, path: number[]): void {
+    if (!node) {
+        return;
+    }
 
+    //recurse
+    walk(node.left, path);
+    walk(node.right, path);
+
+    // post
+    path.push(node.value);
+}
+
+export default function post_order_search(head: BinaryNode<number>): number[] {
+    let path: number[] = [];
+    walk(head, path);
+    return path;
 }
