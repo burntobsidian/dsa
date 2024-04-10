3f83e6895e9df5d2c6ba4034650569f6740dc6ee
burntobsidian <me@burntobsidian.com>
2024-04-10 23:09:48 +0530

    bfs

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,19 @@
-export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+import Queue from "./Queue";
 
+export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const queue = new Queue<BinaryNode<number>>();
+    queue.enqueue(head);
+
+    while (queue.length !== 0) {
+        const curr = queue.deque();
+
+        if (!curr) continue;
+
+        if (curr.value === needle) {
+            return true;
+        }
+        if (curr.left) queue.enqueue(curr.left);
+        if (curr.right) queue.enqueue(curr.right);
+    }
+    return false;
 }
